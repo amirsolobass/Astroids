c403c468aff6c358e9d19686c2ed4f15186cc865
amirsolobass <amirsolobass@gmail.com>
2025-12-10 17:22:27 +0100

    Added player movement, groups

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        pygame.display.flip()   
+        dt = clock.tick(60) / 1000  # Limit to 60 FPS
+        
+        
 
 if __name__ == "__main__":
     main()
